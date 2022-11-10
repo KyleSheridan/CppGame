38c3ad29c468051f46abb8e7707c9f21399a3465
Kyle Sheridan <55580454+KyleSheridan@users.noreply.github.com>
2022-11-10 22:39:05 +0000

    fixed asset loading bug

--- Source/Background.cpp
+++ Source/Background.cpp
@@ -1,10 +1,19 @@
 #include "Background.h"
 
+#include <stdexcept>
+
 void Background::init()
 {
-	SDL_Surface* surface = IMG_Load("Assets/Space.jpg");
-	texture = SDL_CreateTextureFromSurface(Renderer::GetRenderer(), surface);
-	SDL_FreeSurface(surface);
+	try {
+		SDL_Surface* surface = IMG_Load("../Assets/Space.jpg");
+		texture = SDL_CreateTextureFromSurface(Renderer::GetRenderer(), surface);
+		SDL_FreeSurface(surface);
+
+		if (texture == nullptr) throw std::runtime_error("Texture error");
+	}
+	catch(std::exception& e) {
+		std::cout << "\t" << e.what() << "\t" << IMG_GetError() << "\n";
+	}
 }
 
 void Background::update()
